5c998992f0dd4901fdfdbd36479be83e79449ca2
KOBEsphinx242 <ado.coneo@gmail.com>
2021-11-27 21:30:37 -0500

    feat: Add creating task service handler

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,7 +8,12 @@ export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body('title') title: string, @Body('description') description: string): Task {
+    return this.taskService.createTask(title, description);
   }
 }
