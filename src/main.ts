2df86c9974bdc1fab4c28696aba55aac77cf0e51
KOBEsphinx242 <ado.coneo@gmail.com>
2021-12-08 21:42:37 -0500

    feat: Prep repository to deploy

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT;
   await app.listen(port);
 
   logger.log(`Application is running on: ${port}`);
